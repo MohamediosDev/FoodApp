22b97988691eb1f2075a929562a474ff703930d0
Mohamed Sayed <mrdiscover22@gmail.com>
2021-03-29 21:38:38 +0200

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Soda on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
